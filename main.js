cd2cbc3e15e7c2993ea8d4c8fe2fc6ebeffe1a04
ryu2129 <ryu212920@gmail.com>
2021-04-25 23:35:20 +0900

    list click作成

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!',
+    list: ['りんご', 'ばなな', 'いちご'],
+    handleClick: function(event) {
+      alert(event, target)
+    }
+  }
+})
